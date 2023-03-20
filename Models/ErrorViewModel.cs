e55d5330f369e006232fd94992f9e4fcb524a562
Iordache Catalin <iordache.catalin1986@gmail.com>
2023-03-20 13:14:53 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IcSMP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
